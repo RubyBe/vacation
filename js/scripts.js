6753ea3999028b5830c25bcabb8d56f310d4d446
Sid Benavente <sidbenavente@hotmail.com>
2016-06-16 21:49:48 -0700

    build vacation suggestion list

--- js/scripts.js
+++ js/scripts.js
@@ -22,39 +22,61 @@ $(document).ready(function(){
       suggest2 = "Or with your neighbors next door?"
       suggest3 = "Or at a hotel nearby?"
     }
-    // Set first suggestion
+    // Set hot temperature preferences
     if (temp==="hot" && land==="water") {
       suggest1 = "San Diego"
+      suggest2 = "Rio de Janiero"
+      suggest3 = "Veracruz"
     }
     else if (temp==="hot" && land==="bluesky") {
       suggest1 = "Death Valley"
+      suggest2 = "Monterey, Mexico"
+      suggest3 = "Timbucktu"
     }
     else if (temp==="hot" && land==="mountains") {
       suggest1 = "Reno, Nevada";
+      suggest2 = "Yakima, Washington";
+      suggest3 = "Tuscon, Arizona";
     }
     else if (temp==="hot" && land === "urban"){
-      suggest1="Phoenix, Arizona";
+      suggest1 = "Phoenix, Arizona";
+      suggest2 = "Mumbai, India";
+      suggest3 = "Atlanta, Georgia";
     }
 
-    // Set second suggestion
+    // Set cold temperature preferences
     if (temp==="cold" && land==="water") {
-      suggest2 = "Anchorage, Alaska"
+      suggest1 = "Anchorage, Alaska";
+      suggest2 = "Antartica";
+      suggest3 = "Southern tip of Chile";
     }
     else if (temp==="cold" && land==="bluesky") {
-      suggest2 = "Siberia"
+      suggest1 = "Siberia";
+      suggest2 = "North Central Canada";
+      suggest3 = "Cold and Blue";
     }
     else if (temp==="cold" && land==="mountains") {
-      suggest2 = "Everest";
+      suggest1 = "Everest";
+      suggest2 = "Alps";
+      suggest3 = "Mt. Rainier"
     }
     else if (temp==="cold" && land === "urban"){
-      suggest2 = "Moscow";
+      suggest1 = "Moscow";
+      suggest2 = "Montreal";
+      suggest3 = "Cold and Urban";
     }
 
-    // Set third suggestion
+    // set temperate suggestions
     if (temp==="temperate") {
+      suggest1 = "Seattle";
+      suggest2 = "Seattle";
       suggest3 = "Seattle, Washington (that's right, Seattle has it all!)"
     }
-    console.log(suggest1 + ":" + suggest2 + ":" + suggest3);
+
+    // Build display list
+    $("ul#suggestions").prepend("<li>" + suggest1 + "</li>");
+    $("ul#suggestions").prepend("<li>" + suggest2 + "</li>");
+    $("ul#suggestions").prepend("<li>" + suggest3 + "</li>");
   })
 
 
